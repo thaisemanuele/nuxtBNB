30bb5b0d450eb8d21022d1ebddcd5e2152f64615
thaisemanuele <thatitis@gmail.com>
2020-12-19 18:30:34 +0200

    adding metatags

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,15 @@
 export default {
   components: true,
+  head: {
+    titleTemplate: "NuxtBNB | %s",
+    htmlAttrs: {
+      lang: "en"
+    },
+    bodyAttrs: {
+      class: ["bnb-main"]
+    },
+    meta: [{
+      charset: "utf-8"
+    }]
+  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,16 @@
   import homes from '~/data/homes'
 
   export default {
+    head() {
+      return {
+        title: 'Home',
+        meta: [{
+          name: 'description',
+          content: 'Find the perfect place for your next adventure with NuxtBNB!',
+          hid: 'description'
+        }]
+      }
+    },
     data() {
       return {
         homes: homes.slice(0,3)
